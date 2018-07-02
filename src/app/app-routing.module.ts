a00eb198c3047ee77e63ce3cadc0402fa9fdb254
bruce wang <bruce.wang01@sap.com>
2018-07-02 18:14:14 +0800

    test

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import {AuthGuardService} from './service/auth-guard.service';
+import { ErrorComponent } from "./shared/error/error.component";
+import {LoginComponent} from './shared/login/login.component';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'error',
+    component: ErrorComponent
+  },
+  {
+    path: '',
+    canActivate: [AuthGuardService],
+    loadChildren: './pages/pages.module#PagesModule'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
